802ee9e92b697960093b21901f9b30bcaab392a7
Oksana Voshchana <ovosh@softserveinc.com>
2022-01-10 16:17:25 +0200

    dags change

--- dags/dbt.daily.py
+++ dags/dbt.daily.py
@@ -2,15 +2,19 @@ import os
 import json
 
 from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+from airflow.models import Variable
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.utils.dates import datetime
 from airflow.utils.dates import timedelta
+
+
+IMAGE = Variable.get("dbt_test_image")
+
 
 default_args = {
     'owner': 'test',
     'depends_on_past': False,
     'start_date': datetime(2021, 12, 21),
-    'email': ['noreply@astronomer.io'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -24,43 +28,39 @@ dag = DAG(
     schedule_interval=None,
 )
 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 def load_manifest():
-    local_filepath = os.environ.get('DBT_REPO')
+    local_filepath = os.environ.get('MANIFEST_FILE') or f"{ROOT_DIR}/data/manifest.json"
     with open(local_filepath) as f:
         data = json.load(f)
-
     return data
 
-def make_dbt_task(node, dbt_verb):
+
+def make_dbt_task(node: str, dbt_verb: str, dag: DAG) -> KubernetesPodOperator:
     """Returns an Airflow operator either run and test an individual model"""
-    DBT_DIR = os.environ.get('DBT_REPO')
-    PROFILES = f"{DBT_DIR}/airflow_profiles"
-    repo = os.environ.get('DBT_REPO')
-    GLOBAL_CLI_FLAGS = "--no-write-json"
     model = node.split(".")[-1]
 
-    if dbt_verb == "run":
-        dbt_task = BashOperator(
-            task_id=node,
-            bash_command=f"""
-            cd {DBT_DIR} &&
-            dbt {GLOBAL_CLI_FLAGS} {dbt_verb} --target prod --models {model} --profiles-dir {PROFILES}
-            """,
-            dag=dag,
-        )
-
-    elif dbt_verb == "test":
-        node_test = node.replace("model", "test")
-        dbt_task = BashOperator(
-            task_id=node_test,
-            bash_command=f"""
-            cd {DBT_DIR} &&
-            dbt {GLOBAL_CLI_FLAGS} {dbt_verb} --target prod --models {model} --profiles-dir {PROFILES}
-            """,
-            dag=dag,
-        )
-
-    return dbt_task
+    return KubernetesPodOperator(
+        dag=dag,
+        task_id=node,
+        name=node,
+        is_delete_operator_pod=False,
+        image=IMAGE,
+        cmds=[
+            "python3",
+            "execute.py",
+            "--verb", dbt_verb,
+            "--target", "prod",
+            "--models", model
+        ],
+        resources={
+            "request_cpu": 0.5,
+            "limit_cpu": 1,
+            "request_memory": "64Mi",
+            "limit_memory": "512Mi",
+        },
+    )
 
 data = load_manifest()
 
@@ -69,8 +69,8 @@ for node in data["nodes"].keys():
     if node.split(".")[0] == "model":
         node_test = node.replace("model", "test")
 
-        dbt_tasks[node] = make_dbt_task(node, "run")
-        dbt_tasks[node_test] = make_dbt_task(node, "test")
+        dbt_tasks[node] = make_dbt_task(node, "run", dag)
+        dbt_tasks[node_test] = make_dbt_task(node_test, "test", dag)
 
 for node in data["nodes"].keys():
     if node.split(".")[0] == "model":
@@ -85,4 +85,3 @@ for node in data["nodes"].keys():
             upstream_node_type = upstream_node.split(".")[0]
             if upstream_node_type == "model":
                 dbt_tasks[upstream_node] >> dbt_tasks[node]
-
